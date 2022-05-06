d1b57dfe1b1cea817731521bdf8c683a55bf1472
Asma <asmaasaber@Asmaas-MacBook-Pro.local>
2022-05-06 15:58:24 +0200

    binary_search

---
+++  binary_search.js
@@ -0,0 +1,21 @@
+function  binary_search(list, item) {
+  let low = 0;
+  let high = list.length - 1;
+
+  while(low <= high) {
+    const mid = ~~((low + high) / 2);
+    const guess = list[mid];
+    if(guess === item) {
+      return mid;
+    } else if(guess > item) {
+      high = mid;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
+}
+
+// const testingNumbers = [1, 3, 5, 7, 9, 11 , 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51];
+
+// console.log('searching for 21', binary_search(testingNumbers, 21));
