d3a1b3abdc4c18f631864175666fa3c81e4c9fce
Asma <asmaasaber@Asmaas-MacBook-Pro.local>
2022-05-15 05:52:16 +0200

    recursive Binary Search

---  binary_search.js
+++  binary_search.js
@@ -16,6 +16,25 @@ function  binarySearch(list, item) {
   return null;
 }
 
+function recursiveBinarySearch(list, item, start = 0, end = list.length -1) {
+  if(start <= end) {
+    const mid = ~~((start + end) / 2);
+    const guess = list[mid];
+    if(guess === item) {
+      return mid;
+    } else {
+        if(guess > item) {
+          end = mid;
+        } else {
+          start = mid + 1;
+        }
+        return binarySearch(list, item, start, end)
+    }
+  }
+  return -1;
+}
+
 // const testingNumbers = [1, 3, 5, 7, 9, 11 , 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51];
 
 // console.log('searching for 21', binarySearch(testingNumbers, 21));
+
